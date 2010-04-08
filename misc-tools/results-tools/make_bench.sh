5d730ad2099d164f06f1de69b0133c198cc0c496
Mariusz Grad <mariusz.grad@upb.de>
2010-04-08 17:01:59 +0200

    don't recompile apps each time ident method is used. instaed of run-all-bench use bash_threads.

--- misc-tools/results-tools/make_bench.sh
+++ misc-tools/results-tools/make_bench.sh
@@ -3,10 +3,8 @@
 ARCH=virtex
 # ISE_PASS=${PWD}/../b/lib/ISEPass/libLLVMISEPass.so
 ISE_PASS=${PWD}/XCODE_libLLVMISEPass.so
-# OPT_STATS="-stats -time-passes -track-memory"
-OPT_STATS=""
 
-if [ $# != 2 ] ; then
+if [ $# -lt 2 ] ; then
   echo "usage: $0 algorithm dir"
   exit
 fi
@@ -18,21 +16,16 @@ save_pwd=$PWD
 
 # remove first arg which is app name
 shift 2
-
+PARAMS=$*
 
 function run() {
   cd $dir
-  rm -f ${dir}.prof ${dir}_pass.bc
-  make clean && make
 
-  # run necessary optimizations
-  echo "# running opt with passes (optimizing IR with $opt_p)"
-  opt $opt_p  ${dir}.bc -f -o ${dir}_pass.bc  
-  mv ${dir}_pass.bc ${dir}.bc
-
+  # --- run opt and get candidates
   tempfoo=`basename $0`
   TMPFILE=`mktemp /tmp/${tempfoo}.XXXXXX` || exit 1
 
+  echo "# calculating candidates"
    opt \
     -load ${ISE_PASS} \
     -ise \
@@ -40,7 +33,7 @@ function run() {
     -ise-algorithm-stop=true \
     -ise-architecture=${ARCH} \
     -disable-output \
-    $* \
+    $PARAMS \
     ${dir}.bc  2>&1 | grep "processing" |  awk '$13 ~ /[0-9]/ {print $13}' > $TMPFILE
 
 
@@ -54,8 +47,8 @@ function run() {
   rm $TMPFILE
 
 
-  # run ISE pass
- echo "# running opt with ISE pass: $*"
+ # run ISE pass and get benchmarking results
+ echo "# calculating benchmarking results"
   counter=0;
   opt \
     ${OPT_STATS} \
@@ -66,9 +59,11 @@ function run() {
     -ise-algorithm=${ALGO} \
     -ise-architecture=${ARCH} \
     -disable-output \
-    $* \
+    $PARAMS \
     ${dir}.bc  | grep ^[0-9] > $TMPFILE
-   
+  
+
+  # combine benchmarking results with candidate results
    echo -e "Nodes\t, msecs\t\t, it\t, candidate_nodes"
     while read line; do
       echo -e "$line \t ${cand[${counter}]}"
@@ -78,17 +73,12 @@ function run() {
     { $2=sprintf("%.5f",$2)} { print $1 "\t, " $2 "\t, " $3 "\t, " $4 }'
 }
 
-for gcc_o in -O3  ; do
-  for gcc_p in "-funroll-loops" ; do
-    for opt_p in "-std-compile-opts ${OPT_STATS}" ; do
-        echo "CC = llvm-gcc" > Makefile.common
-        echo "CCOPTS = ${gcc_o} ${gcc_p} -emit-llvm" >> Makefile.common
-        echo "" && echo ""
-        echo "app = ${dir}"
-        echo "gcc = ${gcc_o} ${gcc_p} -emit-llvm"
-        echo "opt = ${opt_o} ${opt_p}"
-        run 
-      done
-    done
-done
 
+# ---------------- main() ----------------- #
+        echo "" && echo ""
+        echo "app  = ${dir}"
+        echo "algo = ${ALGO}"
+        echo "arch = ${ARCH}"
+        echo "opt  = ${PARAMS}"
+        echo "gcc = ${gcc_o} ${gcc_p} -emit-llvm"
+        run 
